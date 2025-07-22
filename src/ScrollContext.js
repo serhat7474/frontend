221a9af68db9e771c47c400ed9d6d5286e11ebcb
serhat7474 <yenibinance989@gmail.com>
2025-07-22 18:13:54 +0300

    Update ScrollContext.js

--- src/ScrollContext.js
+++ src/ScrollContext.js
@@ -1,17 +1,28 @@
-// ScrollContext.js
 import React, { createContext, useContext, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollContext = createContext();
 
 export const ScrollProvider = ({ children }) => {
   const rightSectionRef = useRef(null);
+  const location = useLocation();
 
   useEffect(() => {
-    // Sayfa yüklendiğinde scroll'u en üste kaydır
-    if (rightSectionRef.current) {
-      rightSectionRef.current.scrollTop = 0;
-    }
-  }, []);
+    // Rota değiştiğinde veya sayfa yüklendiğinde scroll'u en üste kaydır
+    const scrollToTop = () => {
+      if (rightSectionRef.current) {
+        rightSectionRef.current.scrollTop = 0;
+        rightSectionRef.current.dataset.loaded = "true";
+        console.log(`ScrollContext: ${location.pathname} için sayfa en üste kaydırıldı`);
+      } else {
+        setTimeout(scrollToTop, 100); // Ref mevcut değilse tekrar dene
+      }
+    };
+
+    setTimeout(() => {
+      requestAnimationFrame(scrollToTop);
+    }, 200);
+  }, [location.pathname]); // Rota değişimlerini dinle
 
   const value = {
     rightSectionRef,
